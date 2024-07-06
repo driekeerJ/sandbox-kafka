67aa012cdcca30698c9078348df389be681bb860
jeroenvanderwal <jeroen.van.der.wal@rabobank.nl>
2024-07-06 19:12:02 +0200

    initial commit

---
+++ src/test/java/org/example/sandboxkafka/SandboxKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sandboxkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
